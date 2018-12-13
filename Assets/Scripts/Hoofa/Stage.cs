b5dc449613a2b9ca0d5967d8c4a3111dd685572e
KLD <kld0093@hotmail.com>
2018-12-13 21:20:47 +0300

    Added basic Stage class

---
+++ Assets/Scripts/Hoofa/Stage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Stage : MonoBehaviour {
+
+    public Stage[] NextStages;
+    public GameObject[] Holes;
+    public GameObject[] Monsters;
+
+    protected abstract void StartNextStage(int i);
+    protected abstract void OnMonsterKilled(GameObject monster);
+}
